f9e4c31efa8e43218218a781d248c219d4a044b2
Nicolas Perraut <n.perraut@gmail.com>
2019-10-23 21:05:49 +0200

    Add event description for Grenoble

--- pages/grenoble.vue
+++ pages/grenoble.vue
@@ -15,6 +15,60 @@ div
             a(href="https://www.eventbrite.com/e/startup-weekend-grenoble-9-tickets-76968598149")
               |Je participe
           #eventbrite-widget-container-76968598149.eventbrite
+    .columns
+      .column 
+      .column
+        .full-block.fixed
+          ul
+            li
+              a(href="#section-what")
+                |C'est quoi ?
+            li
+              a(href="#section-who")
+                |C'est pour qui
+  section#section-what
+    .container
+      h3.number-tag #  C'EST QUOI ?
+      p.quote 1 idée pitchée en 1 minute deviendra une startup 54h plus tard...
+      p En 54 heures, vous allez expérimenter les hauts, les bas, les moments d'euphories et de doutes qui jalonnent la création et la vie d'une startup.
+      p En participant au Startup Weekend, vous allez vivre une expérience unique sur la création d'une startup en partant parfois d'une simple idée, et accompagnés par des Coachs expérimentés qui seront là pour vous aider à prendre les 3bonnes décisions, pour que d'une idée puisse émerger une véritable startup !
+      br
+      p.quote Déroulement du week-end
+      p Durant 54 heures, plus de 100 participants - étudiants, professionnels, entrepreneurs, marqueteurs, designers, développeurs, mentors et conférenciers - se réunissent pour créer des business model innovants et convaincre un jury d’experts de la viabilité de leur projet.
+      br
+      .columns
+        .column
+          p.quote Le vendredi
+          p Ceux qui le souhaitent « pitchent » leur idée / projet en 60 secondes : 15 projets sont ensuite sélectionnés par les votes de tous les participants. Les porteurs de projets sélectionnés réunissent autour d’eux une équipe de 4 à 8 personnes pour développer leur idée durant tout le week-end.
+        .column
+          p.quote Le samedi
+          p Du vendredi soir au dimanche, les participants travaillent en équipe sur la faisabilité de leur projet. Des mentors et des Coachs les accompagnent pour les guider, identifier et fiabiliser leur modèle économique, et déterminer la viabilité du projet.
+        .column
+          p.quote Le dimanche
+          p A la fin du weekend, chaque équipe à 5 minutes pour présenter son projet devant un jury d’experts, suivi de 5 minutes de questions / réponses. Les projets les plus prometteurs sont récompensés sur la base de 3 critères : validation client, modèle économique, cohésion d’équipe et l’exécution.
+          br
+  section#who
+      .container
+        h3.number-tag # C'est pour qui ?
+        p Événement ouvert à tous, le Startup Weekend Grenoble a pour leitmotiv la conviction que tout le monde peut créer sa startup et que l’esprit d’équipe est la clef de voûte de tous les projets.
+        p Vous avez une idée ? Venez la présenter le vendredi : vous aurez alors 1 minute pour convaincre les autres participants de voter pour vous et de rejoindre votre équipe !
+        p Vous n'avez pas d'idée ? Aucun soucis ! Le Startup Weekend n'est pas réservé qu'à ceux qui ont des idées : vous rejoindrez une des équipes constituées dès le vendredi soir, et apporterez votre compétence et votre motivation pour faire avancer l'idée et l'amener au stade de projet !
+  section#footer
+    .container-fluid
+      .blue-section
+        .container
+          .columns
+            .column.has-text-left.footer-text
+              | Play Up Alps ® 2018
+            .column
+            .column.has-text-centered
+              img.logo(src="~assets/logo.png")
+              img(src="~assets/logo-title.png")
+            .column
+            .column.footer-text.footer-right
+              a(href="mailto:hello@playupalps.com?subject=Depuis%20le%20site%20internet") 
+                i.far.fa-envelope
+
   script(src="https://www.eventbrite.com/static/widgets/eb_widgets.js")
   script.
     var exampleCallback = function() {
@@ -29,7 +83,7 @@ div
         iframeContainerId: 'eventbrite-widget-container-76968598149',
 
         // Optional
-        iframeContainerHeight: 550,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
+        iframeContainerHeight: 425,  // Widget height in pixels. Defaults to a minimum of 425px if not provided
         onOrderComplete: exampleCallback  // Method called when an order has successfully completed
       });
     });
@@ -48,6 +102,13 @@ export default {
 <style lang="stylus">
 body
   font-family: 'Rubik', sans-serif
+
+ul li
+  display inline
+
+li
+  padding 1rem
+  font-weight bold
 
 .bg-block
   margin-top: 60px
@@ -113,7 +174,7 @@ body
     font-size 4rem
 
   .eventbrite
-    margin-top 50px
+    margin-top 30px
 
 img.bubble
   position: absolute
@@ -134,16 +195,27 @@ img.bubble
 
 h3.number-tag
   font-size: 45px
-  color: #E4003A
+  color: #1078be
   letter-spacing: 0.6px
+  font-weight bold
+  text-transform uppercase
 
 .full-block
   font-size: 18px
   line-height: 28px
   padding: 30px 0 30px 30px
+  &.fixed
+    background white
+    width 100%
+    position absolute
+    bottom 0
 
 .red
   background-color: #E4003A
+  color: #FFFFFF
+
+.blue
+  background-color: #1078be
   color: #FFFFFF
 
 .white
@@ -153,6 +225,9 @@ h3.number-tag
 h4.section-title
   font-size: 35px
   color: #1F2951
+
+p
+  text-align justify
 
 p.quote
   font-size: 24px
